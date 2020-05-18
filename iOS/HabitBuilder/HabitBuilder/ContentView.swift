f60bc80597eec146679b4cfcfbc5b4721edb5cba
anandb7248 <anandb7248@gmail.com>
2020-05-18 13:30:45 -0700

    add iOS starter files

---
+++ iOS/HabitBuilder/HabitBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitBuilder
+//
+//  Created by Anand Batjargal on 5/18/20.
+//  Copyright © 2020 ALM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
