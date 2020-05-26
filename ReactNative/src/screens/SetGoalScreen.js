dcbdc52c5006c3e011d6fc9fe2209bbe07081fb8
anandb7248 <anandb7248@gmail.com>
2020-05-26 15:49:39 -0700

    complete UI of Set Goal Screen

--- ReactNative/src/screens/SetGoalScreen.js
+++ ReactNative/src/screens/SetGoalScreen.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../styles/Colors";
 import PageHeader from "../components/PageHeader";
 import Divider from "../components/Divider";
 import BigButton from "../components/BigButton";
 import moment from "moment";
+import BigTextInput from "../components/BigTextInput";
+import TextLabel from "../components/TextLabel";
+import DatePicker from "../components/DatePicker";
+import { TouchableOpacity } from "react-native";
 
 function SetGoalScreen(props) {
   const today = moment().format("MMM D, YYYY");
@@ -12,9 +16,41 @@ function SetGoalScreen(props) {
   return (
     <View>
       <PageHeader text="Set a Goal"></PageHeader>
-      <Divider />
-      <StartingDate>{today}</StartingDate>
 
+      <Divider />
+      <StartingDate>From Today: {today}</StartingDate>
+      <BigTextInput></BigTextInput>
+      <TextLabel label="When do you want to achieve your goal?" />
+      <DatePicker />
+      <DaysToCompletion>60 days to completion</DaysToCompletion>
+      <Divider />
+
+      <TextLabel label="Daily Habits to Achieve your Goal" />
+      <Container>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Label>Exercise everyday for 1 hour</Label>
+        </TouchableOpacity>
+      </Container>
+      <Container>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Label>Drink a kale smoothie each day</Label>
+        </TouchableOpacity>
+      </Container>
+      <Container>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Label>Do IF with 8 hour eating window</Label>
+        </TouchableOpacity>
+      </Container>
+      <Container>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Label>-</Label>
+        </TouchableOpacity>
+      </Container>
+      <Container>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Label>-</Label>
+        </TouchableOpacity>
+      </Container>
       <BigButton text="Set Goal" />
     </View>
   );
@@ -34,4 +70,24 @@ const StartingDate = styled.Text`
   padding: 20px;
 `;
 
-const Label = styled.Text``;
+const DaysToCompletion = styled.Text`
+  color: ${COLORS.appYelow};
+  font-size: 18px;
+  font-weight: bold;
+  margin: 10px auto;
+`;
+
+const Container = styled.View`
+  background: white;
+  border-radius: 25px;
+  width: 350px;
+  height: 40px;
+  margin: 10px auto;
+`;
+
+const Label = styled.Text`
+  color: ${COLORS.appBlue};
+  font-size: 12px;
+  font-weight: bold;
+  margin: auto auto;
+`;
