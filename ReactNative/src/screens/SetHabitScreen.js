10e9e656b3a444b3019ec420e12bb8f5604ab060
Luis <ltluistrujillo25@gmail.com>
2020-06-01 18:46:36 -0700

    Added states to buttons

--- ReactNative/src/screens/SetHabitScreen.js
+++ ReactNative/src/screens/SetHabitScreen.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-native";
 import styled from "styled-components";
 import COLORS from "../styles/Colors";
 import PageHeader from "../components/PageHeader";
@@ -11,32 +12,82 @@ import SmallButton from "../components/SmallButton";
 import LongButton from "../components/LongButton";
 import Bell from "../components/Bell";
 import ReminderButton from "../components/ReminderButton";
+import DatePicker from "../components/DatePicker";
 
 function SetHabitScreen(props) {
+  const [status, setStatus] = useState("Daily");
+  const [sunday, sundayStatus] = useState(false);
+  const [monday, mondayStatus] = useState(false);
+  const [tuesday, tuesdayStatus] = useState(false);
+  const [wednesday, wednesdayStatus] = useState(false);
+  const [thursday, thursdayStatus] = useState(false);
+  const [friday, fridayStatus] = useState(false);
+  const [saturday, saturdayStatus] = useState(false);
+  const [everyday, everydayStatus] = useState(false);
+
   return (
     <View>
       <PageHeader text="Set a Habit"></PageHeader>
-
       <Divider />
       <Padding>
         <BigTextInput></BigTextInput>
       </Padding>
       <TextLabel label="I want to repeat this..." />
       <ViewHorizontal>
-        <MediumButton text="Daily" color={COLORS.appYelow} />
-        <MediumButton text="Weekly" color={COLORS.appGray} />
+        <MediumButton
+          text="Daily"
+          color={status === "Daily" ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => setStatus("Daily")}
+        />
+        <MediumButton
+          text="Weekly"
+          color={status === "Weekly" ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => setStatus("Weekly")}
+        />
       </ViewHorizontal>
       <ViewHorizontal>
-        <SmallButton text="Su" color={COLORS.appGray} />
-        <SmallButton text="M" color={COLORS.appGray} />
-        <SmallButton text="T" color={COLORS.appGray} />
-        <SmallButton text="W" color={COLORS.appGray} />
-        <SmallButton text="Th" color={COLORS.appGray} />
-        <SmallButton text="F" color={COLORS.appGray} />
-        <SmallButton text="Sa" color={COLORS.appGray} />
+        <SmallButton
+          text="Su"
+          color={sunday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => sundayStatus(!sunday)}
+        />
+        <SmallButton
+          text="M"
+          color={monday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => mondayStatus(!monday)}
+        />
+        <SmallButton
+          text="T"
+          color={tuesday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => tuesdayStatus(!tuesday)}
+        />
+        <SmallButton
+          text="W"
+          color={wednesday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => wednesdayStatus(!wednesday)}
+        />
+        <SmallButton
+          text="Th"
+          color={thursday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => thursdayStatus(!thursday)}
+        />
+        <SmallButton
+          text="F"
+          color={friday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => fridayStatus(!friday)}
+        />
+        <SmallButton
+          text="Sa"
+          color={saturday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => saturdayStatus(!saturday)}
+        />
       </ViewHorizontal>
       <Container>
-        <LongButton text="Everyday" color={COLORS.appYelow} />
+        <LongButton
+          text="Everyday"
+          color={everyday ? COLORS.appYelow : COLORS.appGray}
+          onPress={() => everydayStatus(!everyday)}
+        />
       </Container>
       <Padding />
       <Padding />
