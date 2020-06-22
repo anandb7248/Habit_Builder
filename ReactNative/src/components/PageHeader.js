90e39e54ad6a39a19095c3377fe39ba410591f1c
Griffin <cougargriff@gmail.com>
2020-06-22 04:30:31 -0700

    updated spacing on header

--- ReactNative/src/components/PageHeader.js
+++ ReactNative/src/components/PageHeader.js
@@ -15,7 +15,7 @@ export default PageHeader;
 const HeaderText = styled.Text`
   font-size: ${hp("6%")};
   color: ${COLORS.appYelow};
-  padding: ${(props) => (props.hasHeader ? hp("0%") : hp("5px"))}px 26px 0px;
+  padding: ${(props) => (props.hasHeader ? hp("0%") : hp("6%"))}px 26px 0px;
   margin: 0 auto;
   font-family: "PTSans-Regular";
 `;
--- ReactNative/src/navigator/AppNav.js
+++ ReactNative/src/navigator/AppNav.js
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from "react";
 import TabLayout from "../components/TabLayout";
 import TodayScreen from "../screens/TodayScreen";
 import GoalsScreen from "../screens/GoalsScreen";
+import styled from "styled-components";
+
+const PLACEHOLDER = styled.View``;
+
+/*
+  CONCERNING ICONS
+  Im sourcing these icons from materialdesignicons.com.
+  Just grab the name of the icon and place it in object as 
+  seen below. 
+*/
 
 const AppNav = () => {
   return (
@@ -13,9 +23,19 @@ const AppNav = () => {
           iconName: "account-circle",
         },
         {
+          name: "Calendar",
+          component: PLACEHOLDER,
+          iconName: "calendar",
+        },
+        {
           name: "Goals",
           component: GoalsScreen,
           iconName: "trophy",
+        },
+        {
+          name: "Settings",
+          component: PLACEHOLDER,
+          iconName: "tune",
         },
       ]}
     />
