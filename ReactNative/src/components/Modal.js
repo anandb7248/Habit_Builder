6175e853ed6080f98d527721e3542ac7a67616d0
Luis <ltluistrujillo25@gmail.com>
2020-07-07 14:23:53 -0700

    Modal added & navigates to new screens

---
+++ ReactNative/src/components/Modal.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import ModButton from "./ModButton";
+
+export default ({ navigation }) => (
+  <TouchableOpacity
+    activeOpacity={1}
+    onPress={() => navigation.pop()}
+    style={{
+      flex: 1,
+      backgroundColor: "transparent",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      marginBottom: 75,
+    }}
+  >
+    <ModButton
+      text="New Goals"
+      spacing="3"
+      width="45%"
+      height="7%"
+      fontSize="3%"
+      onPress={() => {
+        navigation.navigate("CreateGoal");
+      }}
+    />
+    <ModButton
+      text="New Habits"
+      spacing="3"
+      width="45%"
+      height="7%"
+      fontSize="3%"
+      onPress={() => {
+        navigation.navigate("CreateHabit");
+      }}
+    />
+  </TouchableOpacity>
+);
